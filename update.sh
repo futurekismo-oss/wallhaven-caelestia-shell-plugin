30c1ca90769a925f5b4449b290697df4e34ca4bd
Futurekismo <futurekismo@gmail.com>
2026-07-16 05:40:58 +0200

    better update script

--- update.sh
+++ update.sh
@@ -1,15 +1,22 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
 APP_NAME="wallhaven-fetch"
 FILE_URL="https://raw.githubusercontent.com/futurekismo-oss/wallhaven-caelestia-shell-plugin/refs/heads/main/wallhaven-fetch"
-INSTALL_PATH="$HOME/.local/bin/$APP_NAME"
+INSTALL_DIR="$HOME/.local/bin"
+INSTALL_PATH="$INSTALL_DIR/$APP_NAME"
 
-echo "Removing old version"
-rm "$HOME/.local/bin/$APP_NAME"
+TMP_DIR=$(mktemp -d)
 
-echo "Downloading new version ..."
-curl -s -L "$FILE_URL" -o "$APP_NAME"
+echo "Downloading new version..."
+curl -s -L "$FILE_URL" -o "$TMP_DIR/$APP_NAME"
 
-chmod u+x "$APP_NAME"
-mkdir -p "$HOME/.local/bin"
-cp "$APP_NAME" "$HOME/.local/bin/"
+chmod u+x "$TMP_DIR/$APP_NAME"
+mkdir -p "$INSTALL_DIR"
+
+echo "Replacing old version..."
+mv "$TMP_DIR/$APP_NAME" "$INSTALL_PATH"
+
+rm -rf "$TMP_DIR"
 echo "Installed to $INSTALL_PATH"
